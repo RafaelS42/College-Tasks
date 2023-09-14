6d5e2a39d5d9a7e13f7b25e7680714b1d5015357
RafaelS42 <127380637+RafaelS42@users.noreply.github.com>
2023-09-14 03:30:21 -0300

    p2 atividade 4

---
+++ Tecnologias para Front End/Atividade 4/Aula.js
@@ -0,0 +1,32 @@
+console.log("Calculo de médias do aluno");
+
+const notas = new Array();
+notas[0] = parseFloat(prompt('Primeira nota ='));
+notas[1] = parseFloat(prompt('Segunda nota  ='));
+notas[2] = parseFloat(prompt('Terceira nota ='));
+
+let media = ((notas[0]+notas[1]+notas[2]) / 3);
+
+alert(" A média é " + media)
+
+if (media >= 7 ) {
+    alert('Aluno passou!')
+} else if (media <= 4 ) {
+    alert('Aluno Reprovou')
+} else {
+    alert('Aluno na recuperação')
+    notas[3] = parseFloat(prompt('Nota da recuperação ='));
+    console.log(((notas[3] + media) / 2 ))
+
+    if (((notas[3] + media) / 2 ) >= 5) {
+        alert('Aluno aprovado')
+    } else {
+        alert('Aluno Reprovado')
+    }
+
+}
+
+
+
+console.log("As nostas são " + notas.toFixed(2));
+alert("A média é " + media )
